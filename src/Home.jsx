7893cf6dd3b02e0e4ab158a5ed8217d308f6803e
Amy Yi Li <142811190+AmyLi12@users.noreply.github.com>
2025-03-03 16:26:25 +0000

    completed activity

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,7 +8,13 @@ import ChatBot from './components/ChatBot'
 import DarkModeSwitcher from './components/DarkModeSwitcher'
 
 export default function Home() {
-  const [businessCardImage, setBusinessCardImage] = useState('/images/cornell-seal-black.svg')
+
+  const[cardData, setCardData] = useState({
+    name:"Ezra Cornell",
+    email:"ezra@cornell.edu",
+    description:"Ezra Cornell co-founded Cornell University in 1865.",
+    image:'/images/cornell-seal-black.svg'
+  });
 
   return (
     <main>
@@ -16,19 +22,22 @@ export default function Home() {
 
       <h2>Business Card</h2>
       <BusinessCard
-        image={businessCardImage}
-        name="Ezra Cornell"
-        email="ezra@cornell.edu"
-        description="Ezra Cornell co-founded Cornell University in 1865."
+        image={cardData.image}
+        name={cardData.name}
+        email={cardData.email}
+        description={cardData.description}
       />
-      <ImageSelector onChange={(url) => setBusinessCardImage(url)} />
+      <ImageSelector
+      onChange{(url) => setCardData((v) => ({...v, image: url}))}
+      // onChange={(url) => setBusinessCardImage(url)}
+      />
       <div className="form-group">
         <label htmlFor="name">Name:</label>
-        <input id="name" />
+        <input id="name" value={cardData.name} onChange={(e) => setCardData((v) => ({...v, name: e.target.value}))}/>
       </div>
       <div className="form-group">
         <label htmlFor="email">Email:</label>
-        <input id="email" type="email" />
+        <input id="email" type="email" value={cardData.email} onChange={(e) => setCardData((v) => ({...v, email: e.target.value}))}/>
       </div>
 
       <h2>Gallery Example</h2>
